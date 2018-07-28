b6373d9673a37f2f3eeaeab641f9f327c964d875
Onaopemipo Aikomo <opemipoaikomo@gmail.com>
2018-07-28 00:42:29 +0000

    Base app structure and background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="app">
+  <div class="app" id="app">
+    <div class="pattern pattern-on-top"></div>
+    <div class="pattern pattern-at-bottom"></div>
+    <index v-if="state === 'index'" @changeState="changeState" />
+    <div v-else>
+      <nav :state="state" />
+      <practice v-if="state === 'practice'" @changeState="changeState" />
+      <performance v-if="state === 'performance'" @changeState="changeState" />
+    </div>
   </div>
 </template>
 
@@ -15,9 +23,55 @@ export default {
     Practice,
     Performance,
   },
+  data() {
+    return {
+      state: 'index',
+    };
+  },
+  methods: {
+    changeState(state) {
+      this.state = state;
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Barlow:400,500,700');
 
+* {
+  box-sizing: border-box;
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(180deg, #D17417 0%, #E7A15B 100%);
+  font-family: 'Barlow', sans-serif;
+}
+
+.pattern {
+  position: absolute;
+  background: url('./assets/japanese-pattern-dark.png');
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 80px;
+
+  &-on-top {
+    top: 0;
+  }
+
+  &-at-bottom {
+    bottom: 0;
+  }
+}
 </style>
+
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="bg">
+    <div class="frame">
+      <div class="title">
+        <h1>Free Pour</h1>
+        <h3>Practice App</h3>
+      </div>
+      <div class="pattern"></div>
+      <div class="actions">
+        <a href="#">Start Practice</a>
+        <a href="#">View Perfomance</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
