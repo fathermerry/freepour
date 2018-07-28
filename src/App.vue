7308240ee12de2388897b59046aa43c3be91e2ba
Onaopemipo Aikomo <opemipoaikomo@gmail.com>
2018-07-28 00:18:13 +0000

    Add base vue + webpack

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import Index from './components/Index';
+import Practice from './components/Practice';
+import Performance from './components/Performance';
+
+export default {
+  name: 'App',
+  components: {
+    Index,
+    Practice,
+    Performance,
+  },
+};
+</script>
+
+<style>
+
+</style>
